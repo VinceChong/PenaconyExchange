a85eda32f9114ed7224bedb853207ceed6057bdb
VinceChong <a03chunwei@gmail.com>
2025-07-25 16:15:04 +0800

    SQL

--- db/sql/category.sql
+++ db/sql/category.sql
@@ -1,7 +1,4 @@
 use PenaconyExchange;
 
--- Insert Publisher
-INSERT INTO Publisher (publisherName) VALUES ("Capcom"), ("Hoyoverse");
-
 -- Insert Categories
 INSERT INTO Category (categoryName) VALUES ("Action"), ("RPG"), ("Turn Based"), ("Single Player"), ("Multiplayer"), ("Horror");
